f3930d07b28de9f6a93d4584b6f43c744ba6b1ff
Bugraalgan <bugraallgan@gmail.com>
2024-05-09 18:12:48 +0300

    rocket finish

--- Assets/Script/kuphareket.cs
+++ Assets/Script/kuphareket.cs
@@ -2,9 +2,10 @@ using System;
 using UnityEngine;
 public class kuphareket : MonoBehaviour
 {
-    [SerializeField] [Range(0,1)] float movementFactor;
+    float movementFactor;
     [SerializeField] Vector3 movementVector;
-     Vector3 startingposition;
+    Vector3 startingposition;
+    [SerializeField] private float period = 2f;
 
     void Start()
     {
@@ -13,6 +14,10 @@ public class kuphareket : MonoBehaviour
 
     void Update()
     {
+        float cycles = Time.time / period;
+        const double tau = Math.PI * 2;
+        double rawSinWawe = Math.Sin(cycles * tau);
+        movementFactor = (float)((rawSinWawe + 1) / 2);
         Vector3 offset = movementFactor * movementVector;
         transform.position=offset+startingposition;
     }
