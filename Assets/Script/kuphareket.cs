13a3884bd292f985d4d01c4f587583fe1b71ec6d
Bugraalgan <bugraallgan@gmail.com>
2024-05-09 21:11:00 +0300

    Rocket game last update

--- Assets/Script/kuphareket.cs
+++ Assets/Script/kuphareket.cs
@@ -17,7 +17,7 @@ public class kuphareket : MonoBehaviour
         float cycles = Time.time / period;
         const double tau = Math.PI * 2;
         double rawSinWawe = Math.Sin(cycles * tau);
-        movementFactor = (float)((rawSinWawe + 1) / 2);
+        movementFactor = (float)(rawSinWawe + 1)/2;
         Vector3 offset = movementFactor * movementVector;
         transform.position=offset+startingposition;
     }
